338b4038ef85b328e4d31395c0f4ada73c0ce7fc
unknown <igor.lirussi@studio.unibo.it>
2021-03-21 14:49:09 +0100

    tests+implem flatmap

---
+++ test/u03/TestMyLists.scala
@@ -0,0 +1,27 @@
+package u03
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TestMyLists {
+
+
+  import MyLists.MyList._
+
+  val lst = Cons(10 , Cons(20 , Cons(30, Nil() )) )
+
+  @Test def testDrop(){
+    val lst = Cons(10 , Cons(20 , Cons(30, Nil() )) )
+    assertEquals( Cons(20, Cons(30, Nil())) , drop(lst,1) )
+    assertEquals( Cons(30, Nil()) , drop(lst,2))
+    assertEquals( Nil(), drop(lst, 5))
+  }
+
+  @Test def testFlatMap(){
+    assertEquals( Cons(11 , Cons(21 , Cons(31 , Nil()))),
+      flatMap( lst )(v => Cons ( v+1 , Nil () )))  // Cons (11 , Cons (21 , Cons (31 , Nil ())))
+    assertEquals( Cons(11 , Cons(12 , Cons(21 , Cons(22 , Cons(31 , Cons(32 , Nil())))))),
+      flatMap ( lst )(v => Cons ( v+1 , Cons (v+2 , Nil () ))))
+    // Cons (11 , Cons (12 , Cons (21 , Cons (22 , Cons (31 , Cons (32 , Nil ()))))))
+  }
+}
